2f281296d3693c595e4daee20de4ff1b123e5e8a
SANA SHAHIN <sanashahin2225@gmail.com>
2022-02-06 13:48:09 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:alpine:3.7
+
+RUN mkdir /site
+
+COPY . /site
+
+WORKDIR /site
+
+ENV USERNAME="Hello World"
+
+ENV PASSWORD="Python Simple Static Website"
+
+RUN pip install -r requirements.txt
+
+EXPOSE 80
+
+ENTRYPOINT ["python"]
+
+CMD ["serve.py"]
---
+++ serve.py
@@ -0,0 +1,15 @@
+from os import environ 
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+app.config['user'] = environ.get('USERNAME')
+app.config['passw'] = environ.get('PASSWORD')
+
+@app.route("/")
+def hello():
+    return render_template('index.html',user=environ.get('USERNAME'),passw=environ.get('PASSWORD'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
